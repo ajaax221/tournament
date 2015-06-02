3b4c812f767e068501f9138d3f6d625ec60f69ad
ajaax221 <ajaax1@gmail.com>
2015-06-02 09:18:14 -0300

    Insert comments

--- tournament.sql
+++ tournament.sql
@@ -5,20 +5,27 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+
+-- Drop, Create and connect to tournament database
 DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 \c tournament
 
-
+-- Drop players and matches tables if they exists
 DROP TABLE IF EXISTS players;
 DROP TABLE IF EXISTS matches;
 
-
+-- Create new table playes with id and name
 CREATE TABLE players (
 	id serial primary key,
 	name TEXT
 	);
 
+-- Create table matches
+-- id, primary key of table
+-- id_player, reference to table players field id
+-- win, counter of victories
+-- matches, counter for how many matches he played
 CREATE TABLE matches (
 	id serial primary key,
 	id_player serial references players(id) ON DELETE CASCADE ON UPDATE CASCADE,
